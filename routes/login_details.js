3e495a200212d27334a36d5be2863f3ebefebe51
roaazoabi <roaazo@edu.hac.ac.il>
2025-01-20 18:56:32 +0200

    Add Cookies and redirections

--- routes/login_details.js
+++ routes/login_details.js
@@ -1,43 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var cookieParser = require('cookie-parser');
+// ------------------------------- Here is the cookie timer in seconds ----------------------------------------------------------
 const REGISTER = 30; 
 router.use(cookieParser());
 router.get('/', function(req, res, next) {
   const registeredDetails = req.cookies['Credintials'];
 
   if (registeredDetails) {
-    try {
       const decodedData = decodeURIComponent(registeredDetails);
       const userDetails = JSON.parse(decodedData);
-      console.log(userDetails)
       res.render('details', {
         title: 'Register',
         errorMessage: null,
-        ...userDetails 
+        user: userDetails,
+        registered: null,
       });
-    } catch (error) {
-      res.render('details', { title: 'Register', errorMessage: null });
-    }
-  } else {
-    res.render('details', { title: 'Register', errorMessage: null });
+  } 
+  else {
+    res.render('details', { title: 'Register', errorMessage: null, user: null, registered: null });
   }
 });
 router.post('/', function (req, res) {
-  const { Email, "First Name": firstName, "Last Name": lastName } = req.body;
+  var { Email, "First Name": firstName, "Last Name": lastName } = req.body;
+  Email = Email.trim();
+  firstName = firstName.trim();
+  lastName = lastName.trim();
   const emailExists = global.users.some(user => user.Email === Email);
+  current = {Email, firstName, lastName}
   if (emailExists) {
     res.render('details', {
         title: 'Registration Error',
         errorMessage: 'This email is already in use, please choose another one.',
+        user: current,
+        registered: null,
     });
   } 
+  else if (!firstName || !lastName){
+    res.render('details', {
+      title: 'Registration Error',
+      errorMessage: 'Some Information are missing!',
+      user: current,
+      registered: null,
+    });
+  }
   else {
     const newUser = { Email, firstName, lastName };
-    global.users.push(newUser);
     const userDataString = encodeURIComponent(JSON.stringify(newUser));
     res.cookie('Credintials', userDataString, { maxAge: REGISTER * 1000, httpOnly: true });
-    res.render('password', { title: 'Password' });
+    res.redirect('/password');
   }
 });
 module.exports = router;
--- routes/login_password.js
+++ routes/login_password.js
@@ -5,26 +5,40 @@ var cookieParser = require('cookie-parser');
 router.use(cookieParser());
 
 router.get('/', function(req, res, next) {
-  const registeredDetails = req.cookies['REGISTER'];
+  const registeredDetails = req.cookies['Credintials'];
   if (registeredDetails) {
-    res.render('password', { title: 'Password', errorMessage: null, registeredDetails });
-  } else {
     res.render('password', { title: 'Password', errorMessage: null });
+  } 
+  else {
+    res.redirect('/')
   }
 });
 
 router.post('/', function(req, res, next) {
-  const { password_1, password_2 } = req.body;
-
-  if (password_1 !== password_2) {
-    res.render('password', {
-      title: 'Registration Error',
-      errorMessage: 'Passwords do not match!',
-    });
-  } 
+  const registeredDetails = req.cookies['Credintials'];
+  if (!registeredDetails)
+    res.render('details', { title: 'Register', errorMessage: null , user: null, registered: null});
   else {
-    res.clearCookie('REGISTER');
-    res.send('Password successfully set!');
+    const { password_1, password_2 } = req.body;
+    if (password_1 !== password_2) {
+      res.render('password', {
+        title: 'Registration Error',
+        errorMessage: 'Passwords do not match - Try Again!',
+      });
+    } 
+    else {
+      const decodedData = decodeURIComponent(registeredDetails);
+      const userDetails = JSON.parse(decodedData);
+      var firstName = userDetails['firstName']
+      var lastName = userDetails['lastName']
+      var Email = userDetails['Email']
+      const newUser = { Email,firstName,lastName, password: password_1};
+      // Add to DB
+      global.users.push(newUser);
+      console.log(global.users);
+      res.clearCookie('Credintials');
+      res.render('details', { title: 'Register', errorMessage: null , user: null, registered: "You are registered!"});
+    }
   }
 });
 
